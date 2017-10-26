ae9e3b3de8e20511fcb678f7eb6be7387972ef24
RafaelSAT <32313536+RafaelSAT@users.noreply.github.com>
2017-10-26 19:02:43 -0200

    Update cadastro.js

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -8,7 +8,7 @@ $(function(){
 			type:'POST',
 			cache:false,
 			data:txt,
-			url:'http://lista_usuarios.pc/models/Cadastro.php',
+			url:'http://projeto_lista_usuarios.pc/models/Cadastro.php',
 			success:function(html){
 				$("h2").html("Cadastro realizado com Sucesso!");
 			},			
